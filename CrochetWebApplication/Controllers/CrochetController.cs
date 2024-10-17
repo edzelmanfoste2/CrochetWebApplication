05a371e0b42d0210d0a412192734d8098f64fab5
Christian Caluya <christianlcaluya@iskolarngbayan.pup.edu.ph>
2024-10-17 14:28:15 +0800

    Add project files.

---
+++ CrochetWebApplication/Controllers/CrochetController.cs
@@ -0,0 +1,101 @@
+﻿using CrochetWebApplication.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace CrochetWebApplication.Controllers
+{
+    public class CrochetController : Controller
+    {
+        // GET: CrochetController
+        public async Task<ActionResult> IndexAsync()
+        {
+            string apiURL = "https://localhost:7279/CrochetManagement/CrochetProducts";
+            List<CrochetProduct> crochetProducts = new List<CrochetProduct>();
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync(apiURL);
+
+                var result = await response.Content.ReadAsStringAsync();
+                crochetProducts = JsonConvert.DeserializeObject<List<CrochetProduct>>(result);
+            }
+            return View(crochetProducts);
+        }
+
+        // GET: CrochetController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: CrochetController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CrochetController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(CrochetProduct crochetProduct)
+        {
+            string apiUrl = "https://localhost:7279/CrochetManagement/CreateProduct";
+            using (HttpClient client = new HttpClient())
+            {
+                StringContent content = new StringContent(JsonConvert.SerializeObject(crochetProduct), Encoding.UTF8, "application/json");
+
+                HttpResponseMessage response = await client.PostAsync(apiUrl, content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            return View(crochetProduct);
+        }
+
+        // GET: CrochetController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: CrochetController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(IndexAsync));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CrochetController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: CrochetController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(IndexAsync));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ CrochetWebApplication/Models/CrochetProduct.cs
@@ -0,0 +1,21 @@
+﻿namespace CrochetWebApplication.Models
+{
+    public class CrochetProduct
+    {
+        public string ID { get; set; }
+
+        public string name { get; set; }
+
+        public string productType { get; set; }
+
+        public string availability { get; set; }
+
+        public string description { get; set; }
+
+        public string category { get; set; }
+
+        public string material { get; set; }
+
+        public string size { get; set; }
+    }
+}
